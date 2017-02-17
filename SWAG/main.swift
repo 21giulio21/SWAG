f4cd6afee6c1ae08ef7324e0c2e47114bffab048
Giulio Tavella <giulioTavella@macbook-pro-di-giulio.local>
2017-02-17 22:19:21 +0100

    Initial Commit

---
+++ SWAG/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SWAG
+//
+//  Created by Giulio Tavella on 17/02/17.
+//  Copyright © 2017 Giulio Tavella. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
